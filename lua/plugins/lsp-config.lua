08eafd6454d1dfd90d1b6c61a8e5430b282f20bb
Doniyorbek Urinboyev <doniyorbek7376@gmail.com>
2025-03-15 18:53:42 +0500

    initial commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls", "pyright", "ts_ls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.basedpyright.setup({
+        capabilities = capabilities,
+        settings = {
+          basedpyright = {
+            typeCheckingMode = "standard",
+          },
+        },
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.opt.smartindent = false
+
